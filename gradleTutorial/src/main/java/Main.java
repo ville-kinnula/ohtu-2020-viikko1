3f690dc203a72428f9196e19fbcecb5d0d27cda7
Ville Kinnula <kinnula.ville@gmail.com>
2020-02-15 13:03:33 +0200

    gradleTutorial

---
+++ gradleTutorial/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("kuka olet: ");
+        String nimi = scanner.nextLine();
+
+        System.out.println("Hello " + nimi);
+    }
+}
