e0b963d3917a38c48148eef140ccc6602cecdc8b
Ville Kinnula <kinnula.ville@gmail.com>
2020-02-17 12:59:18 +0200

    codeclimate config ja gradleTutorialia

--- gradleTutorial/src/main/java/Main.java
+++ gradleTutorial/src/main/java/Main.java
@@ -1,11 +1,13 @@
 import java.util.*;
+import ohtu.Multiplier;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("kuka olet: ");
-        String nimi = scanner.nextLine();
+        Multiplier kolme = new Multiplier(3);
+        System.out.println("anna luku ");
+        int luku = scanner.nextInt();
 
-        System.out.println("Hello " + nimi);
+        System.out.println("luku kertaa kolme on " + kolme.multipliedBy(luku) );
     }
 }
